6d3335498fe8835e7ce0a1021f2fcc75a72ea944
mikael04 <mikael.coletto.eng@gmail.com>
2024-07-05 13:31:10 -0300

    Adicionando aula computacionalç

---
+++ 2-Semestre/Inferencia/Aula Computacional/IC e teste para uma amostra.R
@@ -0,0 +1,90 @@
+
+######################### IC e Teste para uma Amostra #########################
+
+
+# Passo 1: Carregar os pacotes que serao usados
+
+if(!require(dplyr)) install.packages("dplyr") # Instalaçao do pacote caso nao esteja instalado
+library(dplyr)                                # Carregamento do pacote
+
+
+# Passo 2: Carregar o banco de dados
+
+# Importante: selecionar o diretorio de trabalho (working directory)
+# Isso pode ser feito manualmente: Session > Set Working Directory > Choose Directory
+# Ou usando a linha de codigo abaixo:
+# setwd("C:/Users/ferna/Desktop")
+
+dados <- read.csv('Banco de Dados 2.csv', sep = ';', dec = ',',
+                  stringsAsFactors = T, fileEncoding = "latin1") # Carregamento do arquivo csv
+View(dados)                             # Visualizacao dos dados em janela separada
+glimpse(dados)                          # Visualizacao de um resumo dos dados
+
+
+###########################################
+###### Caso de Estimação da média #########
+###########################################
+
+# Passo 3: Verificacao da normalidade dos dados
+#H0: os dados provém da distribuição normal
+# H1: os dados não provém da distribuição normal
+shapiro.test(dados$Altura)
+
+
+# Passo 4 - Intervalo de confiança 
+# Amostra provém da distribuição normal e a variância é desconhecida
+alpha<-0.05
+var_interesse<-dados$Altura 
+n<-length(var_interesse)
+
+(LI<-mean(var_interesse)-qt(1-alpha/2, n-1)*sd(var_interesse)/sqrt(n))
+(LS<-mean(var_interesse)+qt(1-alpha/2, n-1)*sd(var_interesse)/sqrt(n))
+
+
+# Passo 5: Realizacao do teste t para uma amostra com IC
+# H0: mu=167
+# H1: mu!=167
+t.test(dados$Altura, mu = 167, conf.level = 0.95)
+
+
+# Observacao:
+  # O teste bicaudal eh o default; caso deseje unicaudal, necessario incluir:
+    # alternative = "greater" (maior) ou alternative = "less" (menor)
+  # Exemplo: t.test(dados$Altura, mu = 167, alternative = "greater")
+    # Nesse caso, o teste verificara se os dados indicam que e media eh 
+    # maior que a media testada
+
+# Passo 6 (opcional): Visualizacao da distribuicao dos dados
+
+boxplot(dados$Altura, ylab = "Altura (cm)")
+
+###############################################
+# Caso de Estimação da Proporção ##############
+##############################################
+# Passo 1: definir quantos uns foram observados e a estimativa da proporção
+
+nmasculino<-sum(dados$Genero=="M")
+phat<- nmasculino/n
+  
+# Passo 2 - Obter o intervalo de confiança
+alpha<-0.05
+
+(LI<-phat-qnorm(1-alpha/2)*sqrt(phat*(1-phat)/(n-1)))
+(LS<-phat+qnorm(1-alpha/2)*sqrt(phat*(1-phat)/(n-1)))
+
+# Passo 3 -  Realizacao do teste da proporção para uma amostra com IC
+prop.test(nmasculino, n=n, p=0.6)
+
+# Proporção das pessoas que tem Ensino superior como Grau de instrução
+nsuperior<-sum(dados$Grau_de_Instruçao=="Superior")
+phat<- nsuperior/n
+
+# Passo 2 - Obter o intervalo de confiança
+alpha<-0.05
+
+(LI<-phat-qnorm(1-alpha/2)*sqrt(phat*(1-phat)/(n-1)))
+(LS<-phat+qnorm(1-alpha/2)*sqrt(phat*(1-phat)/(n-1)))
+
+# Passo 3 -  Realizacao do teste da proporção para uma amostra com IC
+prop.test(nsuperior, n=n, p=0.5)
+
---
+++ 2-Semestre/Inferencia/Aula Computacional/Teste t para amostras independentes.R
@@ -0,0 +1,70 @@
+
+######################### Teste t para Amostras Independentes #########################
+
+
+# Passo 1: Carregar os pacotes que ser?o usados
+
+if(!require(dplyr)) install.packages("dplyr") # Instalacao do pacote caso nao esteja instalado
+library(dplyr)                                # Carregamento do pacote
+if(!require(RVAideMemoire)) install.packages("RVAideMemoire") # Instalacao do pacote caso nao esteja instalado
+library(RVAideMemoire)                                        # Carregamento do pacote
+if(!require(car)) install.packages("car") # Instalacao do pacote caso nao esteja instalado
+library(car)                                # Carregamento do pacote
+
+# Passo 2: Carregar o banco de dados
+
+# Importante: selecionar o diretorio de trabalho (working directory)
+# Isso pode ser feito manualmente: Session > Set Working Directory > Choose Directory
+# Ou usando a linha de c?digo abaixo:
+# setwd("C:/Users/ferna/Desktop")
+
+dados <- read.csv('Banco de Dados 3.csv', sep = ';', dec = ',',
+                  stringsAsFactors = T, fileEncoding = "latin1")  # Carregamento do arquivo csv
+View(dados)                                       # Visualizacao dos dados em janela separada
+glimpse(dados)                                    # Visualizacao de um resumo dos dados
+
+
+# Passo 3: Verificacao da normalidade dos dados
+## Shapiro por grupo (pacote RVAideMemoire)
+#H0: Os dados são normais
+#H1: Os dados não são normais
+byf.shapiro(Nota_Biol ~ Posicao_Sala, dados)
+byf.shapiro(Nota_Fis ~ Posicao_Sala, dados)
+byf.shapiro(Nota_Hist ~ Posicao_Sala, dados)
+
+
+# Passo 4: Verificacao da igualdade variancias
+## Teste de igualdade de variâncias
+x<-dados%>%
+  filter(Posicao_Sala == "Fundos")
+y<-dados%>%
+  filter(Posicao_Sala == "Frente")
+
+#H0: As variâncias dos grupos são iguais
+#H1: As variâncias dos grupos não são iguais
+var.test(x$Nota_Biol,y$Nota_Biol)
+var.test(x$Nota_Fis,y$Nota_Fis)
+var.test(y$Nota_Hist,x$Nota_Hist)
+
+# Passo 5: Realizacao do teste t para amostras independentes
+#H0: As médias dos grupos são iguais
+#H1: As médias dos grupos não são iguais
+t.test(Nota_Biol ~ Posicao_Sala, dados, var.equal=TRUE)
+t.test(Nota_Fis ~ Posicao_Sala, dados, var.equal=FALSE)
+t.test(Nota_Hist ~ Posicao_Sala, dados, var.equal=FALSE)
+
+# Observacao:
+  # O teste bicaudal eh o default; caso deseje unicaudal, necessario incluir:
+    # alternative = "greater" ou alternative = "less"
+  # Exemplo: t.test(Nota_Biol ~ Posicao_Sala, dados, var.equal=TRUE, alternative="greater")
+    # Nesse caso, o teste verificara se a media do primeiro grupo é maior que a media do segundo
+      # O R esta considerando "Frente" como primeiro grupo
+
+
+# Passo 6 (opcional): Visualizacao da distribuicao dos dados
+
+par(mfrow=c(1,3)) # Estabeleci que quero que os graficos saiam na mesma linha
+boxplot(Nota_Biol ~ Posicao_Sala, data = dados, ylab="Notas de Biologia", xlab="Posição na Sala")
+boxplot(Nota_Fis ~ Posicao_Sala, data = dados, ylab="Notas de Fisica", xlab="Posição na Sala")
+boxplot(Nota_Hist ~ Posicao_Sala, data = dados, ylab="Notas de Historia", xlab="Posição na Sala")
+
