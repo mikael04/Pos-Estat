9ee8c24b5aaba139ca2f98cb565622ec0145ff25
mikael04 <mikael.coletto.eng@gmail.com>
2024-10-04 15:29:33 -0300

    Adicionando controle de especificação;

--- 3-Semestre/CEP-ControleEstatisticodoProcesso/Aula03.R
+++ 3-Semestre/CEP-ControleEstatisticodoProcesso/Aula03.R
@@ -74,3 +74,17 @@ x = c(33.75, 33.05, 34, 33.81, 33.46, 34.02, 33.68, 33.27, 33.49, 33.20,
       33.62, 33.00, 33.54, 33.12, 33.84)
 q1 = qcc(x, type="xbar.one")
 q2 = qcc(dados$x, type="xbar.one", std.dev = "SD")
+
+
+## Exemplo controle de especificação
+## Uma corrente tem especificação 100 +- 10 miliamperes
+
+LIC <- 100-10
+LSC <- 100+10
+media <-107
+dp <- 1.5
+
+Cp = (LSC - LIC) / (6 * dp)
+# Resultado Cp -> 2.22 > 1.33, processo é capaz de atender a especificação
+Cpk = min(LSC-media, media-LIC) / (3 * dp)
+# Resultado Cpk -> 0.67 < 1.33, processo é incapaz de atender a especificação
